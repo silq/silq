e2bff7b389feaac8fc01ba7adb30fb74c9e9c335
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-02-08 11:46:05 -0200

    Redirecionando para principal ao logar

--- src/main/webapp/scripts/app/account/login/login.controller.js
+++ src/main/webapp/scripts/app/account/login/login.controller.js
@@ -4,9 +4,9 @@ angular.module('silq2App')
     .controller('LoginController', function ($rootScope, $scope, $state, $timeout, Auth) {
         $scope.user = {};
         $scope.errors = {};
-
         $scope.rememberMe = true;
         $timeout(function (){angular.element('[ng-model="email"]').focus();});
+        
         $scope.login = function (event) {
             event.preventDefault();
             Auth.login({
@@ -15,10 +15,7 @@ angular.module('silq2App')
                 rememberMe: $scope.rememberMe
             }).then(function () {
                 $scope.authenticationError = false;
-                // Forçamos o reload para a atualização do MainController
-                // e suas variavéis $scope de usuário no caso da caixa de login
-                // estar na página inicial
-                $state.go('home', {}, {reload: true});
+                $state.go('principal', {}, {reload: true});
             }).catch(function () {
                 $scope.authenticationError = true;
             });
