b640fd3eed6fadda9928acaf51fdc08c0c31f936
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-03-13 13:01:13 -0300

    Mais testes para LattesParser

--- src/test/java/br/ufsc/silq/core/parser/LattesParserTest.java
+++ src/test/java/br/ufsc/silq/core/parser/LattesParserTest.java
@@ -1,18 +1,20 @@
 package br.ufsc.silq.core.parser;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
 import java.text.ParseException;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import br.ufsc.silq.core.exceptions.SilqErrorException;
 import br.ufsc.silq.core.parser.dto.DadosGeraisResult;
+import br.ufsc.silq.core.parser.dto.PesquisadorResult;
 
 public class LattesParserTest {
 
 	public static final File CURRICULUM_CHRISTIANE = new File("src/test/resources/fixtures/curricula/christiane.xml");
+	public static final File CURRICULUM_RAUL = new File("src/test/resources/fixtures/curricula/raul.xml");
+	public static final File CURRICULUM_RONALDO = new File("src/test/resources/fixtures/curricula/ronaldo.xml");
 
 	private LattesParser lattesParser = new LattesParser();
 
@@ -21,11 +23,26 @@ public class LattesParserTest {
 		DadosGeraisResult dados = this.lattesParser.parseCurriculaDadosGerais(CURRICULUM_CHRISTIANE);
 		// System.out.println(dados);
 
-		assertEquals("Christiane Anneliese Gresse von Wangenheim", dados.getNome());
-		assertEquals("Ciência da Computação - Ciências Exatas e da Terra",
-				dados.getAreaGrandeAreaConhecimento().toString());
-		assertEquals("Metodologia e Técnicas da Computação", dados.getNomeSubAreaConhecimento());
-		assertEquals("3879944876244096", dados.getIdCurriculo());
+		Assertions.assertThat(dados.getNome()).isEqualTo("Christiane Anneliese Gresse von Wangenheim");
+		Assertions.assertThat(dados.getAreaGrandeAreaConhecimento().toString())
+				.isEqualTo("Ciência da Computação - Ciências Exatas e da Terra");
+		Assertions.assertThat(dados.getNomeSubAreaConhecimento()).isEqualTo("Metodologia e Técnicas da Computação");
+		Assertions.assertThat(dados.getIdCurriculo()).isEqualTo("3879944876244096");
+
+		Assertions.assertThat(this.lattesParser.parseCurriculaDadosGerais(CURRICULUM_RAUL)).isNotNull();
+		Assertions.assertThat(this.lattesParser.parseCurriculaDadosGerais(CURRICULUM_RONALDO)).isNotNull();
+	}
+
+	@Test
+	public void testParseCurriculaPesquisador() throws SilqErrorException {
+		PesquisadorResult result = this.lattesParser.parseCurriculaPesquisador(CURRICULUM_RAUL);
+		// System.out.println(result);
+
+		Assertions.assertThat(result.getNome()).isEqualTo("Raul Sidnei Wazlawick");
+		Assertions.assertThat(result.getIdCurriculo()).isEqualTo(7541399131195077L);
+
+		Assertions.assertThat(this.lattesParser.parseCurriculaPesquisador(CURRICULUM_CHRISTIANE)).isNotNull();
+		Assertions.assertThat(this.lattesParser.parseCurriculaPesquisador(CURRICULUM_RONALDO)).isNotNull();
 	}
 
 }
