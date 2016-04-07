554492b1d6b8a5d66cd7a771f350d13fe61ea7cf
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-04-06 21:57:21 -0300

    Getter para originalException em SilqLattesException

--- src/main/java/br/ufsc/silq/core/exception/SilqLattesException.java
+++ src/main/java/br/ufsc/silq/core/exception/SilqLattesException.java
@@ -1,11 +1,14 @@
 package br.ufsc.silq.core.exception;
 
+import lombok.Getter;
+
 public class SilqLattesException extends SilqException {
 	private static final long serialVersionUID = 605392058128899205L;
 
 	/**
 	 * Exception que originou esta
 	 */
+	@Getter
 	private Exception originalException;
 
 	public SilqLattesException(Exception originalException, String msg) {
