bb06c941a8a59153dfd222dc7a3e406cdb918b1d
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-04-11 23:08:50 -0300

    Add QualisEventoRepository

---
+++ src/main/java/br/ufsc/silq/core/business/repository/QualisEventoRepository.java
@@ -0,0 +1,9 @@
+package br.ufsc.silq.core.business.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufsc.silq.core.business.entities.QualisEvento;
+
+public interface QualisEventoRepository extends JpaRepository<QualisEvento, Long> {
+
+}
