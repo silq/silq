8bb4403276ba42ec6a35bf9b56419661cb9193cd
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-05-17 22:55:49 -0300

    Mais testes de reuso de currículo lattes

--- src/test/java/br/ufsc/silq/WebContextTest.java
+++ src/test/java/br/ufsc/silq/WebContextTest.java
@@ -1,12 +1,22 @@
 package br.ufsc.silq;
 
+import javax.inject.Inject;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import br.ufsc.silq.core.forms.usuario.RegisterForm;
+import br.ufsc.silq.core.persistence.entities.Usuario;
+import br.ufsc.silq.core.service.UsuarioService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = { Application.class })
@@ -15,5 +25,30 @@ import org.springframework.transaction.annotation.Transactional;
 @ActiveProfiles(resolver = TestProfilesResolver.class)
 @Transactional
 public abstract class WebContextTest {
+
+	@Inject
+	private UsuarioService usuarioService;
+
+	@Inject
+	private AuthenticationManager authenticationManager;
+
+	/**
+	 * Cria um novo usuário na base de dados e o loga no sistema, para testes.
+	 *
+	 * @return
+	 */
+	protected Usuario loginUser() {
+		RegisterForm registerForm = new RegisterForm();
+		registerForm.setNome("Bruce Wayne");
+		registerForm.setEmail("batman@batman.com");
+		registerForm.setSenha("j0k3r");
+		Usuario user = this.usuarioService.registerUsuario(registerForm);
+
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(registerForm.getEmail(), registerForm.getSenha());
+		Authentication authentication = this.authenticationManager.authenticate(token);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		return user;
+	}
 
 }
--- src/test/java/br/ufsc/silq/core/service/CurriculumLattesServiceTest.java
+++ src/test/java/br/ufsc/silq/core/service/CurriculumLattesServiceTest.java
@@ -9,11 +9,18 @@ import br.ufsc.silq.Fixtures;
 import br.ufsc.silq.WebContextTest;
 import br.ufsc.silq.core.exception.SilqException;
 import br.ufsc.silq.core.persistence.entities.CurriculumLattes;
+import br.ufsc.silq.core.persistence.repository.CurriculumLattesRepository;
 
 public class CurriculumLattesServiceTest extends WebContextTest {
 
 	@Inject
 	private CurriculumLattesService curriculumService;
+
+	@Inject
+	private CurriculumLattesRepository curriculumRepository;
+
+	@Inject
+	private UsuarioService usuarioService;
 
 	@Test
 	public void testSaveComReuso() throws SilqException {
@@ -22,4 +29,29 @@ public class CurriculumLattesServiceTest extends WebContextTest {
 		Assertions.assertThat(lattes2.getId()).isEqualTo(lattes1.getId());
 	}
 
+	@Test
+	public void testIsCurriculumEmUso() throws SilqException {
+		CurriculumLattes lattes1 = this.curriculumService.saveFromUpload(Fixtures.CHRISTIANE_ZIP_UPLOAD);
+		Assertions.assertThat(this.curriculumService.isCurriculumEmUso(lattes1)).isFalse();
+
+		this.loginUser();
+		CurriculumLattes lattes2 = this.usuarioService.saveCurriculumUsuarioLogado(Fixtures.RAUL_ZIP_UPLOAD);
+		Assertions.assertThat(this.curriculumService.isCurriculumEmUso(lattes2)).isTrue();
+
+		this.usuarioService.removeCurriculumUsuarioLogado();
+		Assertions.assertThat(this.curriculumService.isCurriculumEmUso(lattes2)).isFalse();
+	}
+
+	@Test
+	public void testReleaseCurriculum() throws SilqException {
+		this.loginUser();
+		CurriculumLattes lattes2 = this.usuarioService.saveCurriculumUsuarioLogado(Fixtures.RAUL_ZIP_UPLOAD);
+
+		Assertions.assertThat(this.curriculumRepository.count()).isEqualTo(1);
+		this.curriculumService.releaseCurriculum(lattes2); // Não deve remover: currículo ainda em uso
+		Assertions.assertThat(this.curriculumRepository.count()).isEqualTo(1);
+
+		this.usuarioService.removeCurriculumUsuarioLogado();
+		Assertions.assertThat(this.curriculumRepository.count()).isEqualTo(0);
+	}
 }
