c054561dbd82174fb66b7c539a6c186d553f7919
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-06-14 18:35:30 -0300

    Add profile 'test' aos testes unitários

--- src/test/java/br/ufsc/silq/test/TestProfilesResolver.java
+++ src/test/java/br/ufsc/silq/test/TestProfilesResolver.java
@@ -8,7 +8,7 @@ public class TestProfilesResolver implements ActiveProfilesResolver {
 
 	@Override
 	public String[] resolve(Class<?> arg0) {
-		String[] defaultProfiles = { Constants.SPRING_PROFILE_TEST };
+		String[] defaultProfiles = { Constants.SPRING_PROFILE_TEST, Constants.SPRING_PROFILE_FAST };
 		String profiles = System.getProperty("spring.profiles.active");
 
 		if (profiles != null) {
