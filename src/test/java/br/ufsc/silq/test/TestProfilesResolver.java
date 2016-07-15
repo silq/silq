973307bbab83926b29d882dc3a6cc541ff85bbe9
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-07-15 18:50:24 -0300

    Define default test profiles as constant

--- src/test/java/br/ufsc/silq/test/TestProfilesResolver.java
+++ src/test/java/br/ufsc/silq/test/TestProfilesResolver.java
@@ -5,10 +5,13 @@ import org.springframework.test.context.ActiveProfilesResolver;
 import br.ufsc.silq.config.Constants;
 
 public class TestProfilesResolver implements ActiveProfilesResolver {
+	public static String[] defaultProfiles = {
+			Constants.SPRING_PROFILE_TEST,
+			Constants.SPRING_PROFILE_FAST
+	};
 
 	@Override
 	public String[] resolve(Class<?> arg0) {
-		String[] defaultProfiles = { Constants.SPRING_PROFILE_TEST, Constants.SPRING_PROFILE_FAST };
 		String profiles = System.getProperty("spring.profiles.active");
 
 		if (profiles != null) {
