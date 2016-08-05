d903c78140dd1287349259befe3c1052418bb92d
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-08-04 21:57:25 -0300

    Corrigido bug da similaridade não alterada

--- src/main/java/br/ufsc/silq/core/service/AvaliacaoService.java
+++ src/main/java/br/ufsc/silq/core/service/AvaliacaoService.java
@@ -53,6 +53,13 @@ public class AvaliacaoService {
 	private LattesParser lattesParser;
 
 	/**
+	 * Nível de similaridade atual sendo usado pelo serviço.
+	 * Ao setar um novo threshold via {@link #setSimilarityThreshold(Float)} guardamos o valor setado aqui
+	 * para que novas chamadas a este método não resultem em queries desnecessárias caso o valor não tenha sido alterado.
+	 */
+	private Float similarityThreshold = NivelSimilaridade.NORMAL.getValue();
+
+	/**
 	 * Avalia um currículo lattes.
 	 *
 	 * @param lattes Currículo a ser avaliado.
@@ -191,16 +198,21 @@ public class AvaliacaoService {
 	 * Seta o nível de similaridade mínimo (threshold) que será usado para as queries de similaridade no banco.
 	 *
 	 * @param value Valor numérico de 0 a 1 representando o threshold de similaridade.
-	 * @return The value set.
+	 * @return Verdadeiro caso o valor tenha sido alterado ou falso caso não precisou ser alterado pois
+	 *         a avaliação anterior já setou este mesmo valor.
 	 */
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
-	public Float setSimilarityThreshold(Float value) {
-		// TODO (bonetti): bug da similaridade que não se altera
-		Query query = this.em.createNativeQuery("SELECT set_limit(?1)");
-		query.setParameter(1, value);
-		Float result = (Float) query.getSingleResult();
-		log.trace("Similarity threshold set to {}", result);
-		return result;
+	public boolean setSimilarityThreshold(Float value) {
+		if (!value.equals(this.similarityThreshold)) {
+			Query query = this.em.createNativeQuery("SELECT set_limit(?1)");
+			query.setParameter(1, value);
+			Float result = (Float) query.getSingleResult();
+			log.trace("Similarity threshold set to {}", result);
+			this.similarityThreshold = result;
+			return true;
+		}
+
+		return false;
 	}
 
 	/**
