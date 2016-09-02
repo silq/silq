7fcf0d652f59ef97f868c372aecd327e3a0952e0
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-09-01 23:22:33 -0300

    Fix feedback em avalição usando QualisModalTable

--- src/main/webapp/scripts/components/avaliar/avaliar-result-item.directive.js
+++ src/main/webapp/scripts/components/avaliar/avaliar-result-item.directive.js
@@ -19,7 +19,11 @@ angular.module('silq2App')
                 $scope.sugerir = function(item) {
                     QualisModal.open(item, $scope.form)
                         .then(function(result) {
-                            $scope.feedback(item, result.resultado);
+                            var conceito = result.resultado;
+                            conceito.similaridade = result.similaridade.value;
+                            conceito.tituloVeiculo = conceito.titulo;
+                            conceito.conceito = conceito.estrato;
+                            $scope.feedback(item, conceito);
                         });
                 };
 
@@ -29,8 +33,10 @@ angular.module('silq2App')
                     var feedbackRequest;
 
                     // Remove o conceito do feedback e o insere novamente no início da lista
-                    $scope.item.conceitos[0].flagged = false;
-                    $scope.item.conceitos.splice($scope.item.conceitos.indexOf(conceito), 1);
+                    if ($scope.item.conceitos.length > 0) {
+                        $scope.item.conceitos[0].flagged = false;
+                        $scope.item.conceitos.splice($scope.item.conceitos.indexOf(conceito), 1);
+                    }
                     $scope.item.conceitos.unshift(conceito);
                     conceito.flagged = true;
 
