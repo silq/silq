f619f5a0379e121b0cbdac58470f8f56de9232ab
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-09-10 14:44:42 -0300

    Teste de getConceito para FeedbackService

--- src/test/java/br/ufsc/silq/core/service/FeedbackServiceTest.java
+++ src/test/java/br/ufsc/silq/core/service/FeedbackServiceTest.java
@@ -1,6 +1,7 @@
 package br.ufsc.silq.core.service;
 
 import java.util.Date;
+import java.util.Optional;
 
 import javax.inject.Inject;
 
@@ -8,6 +9,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
+import br.ufsc.silq.core.data.Conceito;
 import br.ufsc.silq.core.forms.FeedbackEventoForm;
 import br.ufsc.silq.core.forms.FeedbackPeriodicoForm;
 import br.ufsc.silq.core.persistence.entities.FeedbackEvento;
@@ -105,5 +107,31 @@ public class FeedbackServiceTest extends WebContextTest {
 		Assertions.assertThat(feedback2.getUsuario()).isEqualTo(this.usuarioLogado);
 		Assertions.assertThat(feedback2.getPeriodico()).isEqualTo(outroPeriodico);
 		Assertions.assertThat(feedback2.getDate()).isCloseTo(new Date(), 1000);
+	}
+
+	@Test
+	public void testGetConceitoFeedbackPeriodico() {
+		this.feedbackService.sugerirMatchingPeriodico(this.feedbackPeriodicoForm);
+		Optional<Conceito> conceito = this.feedbackService.getConceitoFeedbackPeriodico(this.feedbackPeriodicoForm.getQuery(), this.usuarioLogado);
+
+		Assertions.assertThat(conceito).isPresent();
+		Assertions.assertThat(conceito.get().getId()).isEqualTo(this.feedbackPeriodicoForm.getPeriodicoId());
+		Assertions.assertThat(conceito.get().getTituloVeiculo()).isEqualTo(this.periodico.getTitulo());
+		Assertions.assertThat(conceito.get().getConceito()).isEqualTo(this.periodico.getEstrato());
+		Assertions.assertThat(conceito.get().getAno()).isEqualTo(this.periodico.getAno());
+		Assertions.assertThat(conceito.get().isFlagged()).isTrue();
+	}
+
+	@Test
+	public void testGetConceitoFeedbackEvento() {
+		this.feedbackService.sugerirMatchingEvento(this.feedbackEventoForm);
+		Optional<Conceito> conceito = this.feedbackService.getConceitoFeedbackEvento(this.feedbackEventoForm.getQuery(), this.usuarioLogado);
+
+		Assertions.assertThat(conceito).isPresent();
+		Assertions.assertThat(conceito.get().getId()).isEqualTo(this.feedbackEventoForm.getEventoId());
+		Assertions.assertThat(conceito.get().getTituloVeiculo()).isEqualTo(this.evento.getTitulo());
+		Assertions.assertThat(conceito.get().getConceito()).isEqualTo(this.evento.getEstrato());
+		Assertions.assertThat(conceito.get().getAno()).isEqualTo(this.evento.getAno());
+		Assertions.assertThat(conceito.get().isFlagged()).isTrue();
 	}
 }
