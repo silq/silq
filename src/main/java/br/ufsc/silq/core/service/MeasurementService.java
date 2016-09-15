ed699bc8e7092c15120ecfc829f9c9a48a1783cf
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-09-15 11:12:54 -0300

    Mais testes para MeasurementService

--- src/main/java/br/ufsc/silq/core/service/MeasurementService.java
+++ src/main/java/br/ufsc/silq/core/service/MeasurementService.java
@@ -6,8 +6,6 @@ import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +19,7 @@ import br.ufsc.silq.core.persistence.entities.QualisEvento;
 import br.ufsc.silq.core.persistence.entities.Usuario;
 import br.ufsc.silq.core.persistence.repository.FeedbackEventoRepository;
 import br.ufsc.silq.core.service.SimilarityService.TipoAvaliacao;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Realiza medições de valores de validação do desempenho e acurácia do algoritmo de avaliação do sistema, incluindo Precisão e Revocação.
@@ -53,44 +52,47 @@ public class MeasurementService {
 		avaliarForm.setNivelSimilaridade(threshold);
 
 		MeasurementResult result = new MeasurementResult(threshold);
-
-		feedbacksEventos.forEach(feedback -> {
-			try {
-				double precision = 0;
-				double recall = 0;
-				boolean match = false;
-
-				QualisEvento eventoFeedback = feedback.getEvento();
-
-				avaliarForm.setArea(eventoFeedback.getAreaAvaliacao()); // TODO: se área do feedback for diferente da área do evento?
-
-				Trabalho trabalho = new Trabalho("", feedback.getAno(), feedback.getQuery()); // O Título do trabalho é ignorado pela avaliação
-
-				List<Conceito> conceitos = this.similarityService.getConceitos(trabalho, avaliarForm, TipoAvaliacao.EVENTO);
-				if (conceitos.isEmpty()) {
-					// Sistema não encontrou nada
-					// Todos os valores ficam 0!
-				} else {
-					Conceito chosen = conceitos.get(0);
-					if (chosen.getId().equals(eventoFeedback.getId())) {
-						match = true;
-					}
-
-					List<Long> ids = conceitos.stream().map(c -> c.getId()).collect(Collectors.toList());
-					if (ids.contains(eventoFeedback.getId())) {
-						// Sistema retornou o resultado real!
-						precision = 1.0 / ids.size();
-						recall = 1.0;
-					}
-				}
-
-				result.addResult(match, precision, recall);
-			} catch (SQLException e) {
-				log.error("Erro ao realizar medição", e);
-			}
-		});
-
+		feedbacksEventos.forEach(feedback -> this.measureFeedback(feedback, result, avaliarForm));
 		return result;
+	}
+
+	private void measureFeedback(FeedbackEvento feedback, MeasurementResult result, AvaliarForm avaliarForm) {
+		double precision = 0;
+		double recall = 0;
+		boolean match = false;
+
+		QualisEvento eventoFeedback = feedback.getEvento();
+
+		avaliarForm.setArea(eventoFeedback.getAreaAvaliacao()); // TODO: se área do feedback for diferente da área do evento?
+
+		Trabalho trabalho = new Trabalho("", feedback.getAno(), feedback.getQuery()); // O Título do trabalho é ignorado pela avaliação
+
+		List<Conceito> conceitos;
+		try {
+			conceitos = this.similarityService.getConceitos(trabalho, avaliarForm, TipoAvaliacao.EVENTO);
+		} catch (SQLException e) {
+			log.error("Erro ao realizar medição do feedback: " + feedback, e);
+			return;
+		}
+
+		if (conceitos.isEmpty()) {
+			// Sistema não encontrou nada
+			// Todos os valores ficam 0!
+		} else {
+			Conceito chosen = conceitos.get(0);
+			if (chosen.getId().equals(eventoFeedback.getId())) {
+				match = true;
+			}
+
+			List<Long> ids = conceitos.stream().map(c -> c.getId()).collect(Collectors.toList());
+			if (ids.contains(eventoFeedback.getId())) {
+				// Sistema retornou o resultado real!
+				precision = 1.0 / ids.size();
+				recall = 1.0;
+			}
+		}
+
+		result.addResult(match, precision, recall);
 	}
 
 }
--- src/test/java/br/ufsc/silq/core/service/MeasurementServiceTest.java
+++ src/test/java/br/ufsc/silq/core/service/MeasurementServiceTest.java
@@ -2,6 +2,8 @@ package br.ufsc.silq.core.service;
 
 import javax.inject.Inject;
 
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Offset;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,13 +29,56 @@ public class MeasurementServiceTest extends WebContextTest {
 	}
 
 	@Test
-	public void testPrecisionAndRecall() {
+	public void testMeasureFeedbackExactMatch() {
+		this.feedbackService.sugerirMatchingEvento(new FeedbackEventoForm(2L, "symposium 3d user interfaces", 2010));
+
+		MeasurementResult result = this.measurementService.measure(this.usuarioLogado, NivelSimilaridade.NORMAL);
+		// result.debug();
+
+		Assertions.assertThat(result.getMatches().get(0)).isEqualTo(true);
+		Assertions.assertThat(result.getPrecisions().get(0)).isEqualTo(1);
+		Assertions.assertThat(result.getRecalls().get(0)).isEqualTo(1);
+
+		Assertions.assertThat(result.size()).isEqualTo(1);
+		Assertions.assertThat(result.threshold()).isEqualTo(NivelSimilaridade.NORMAL.getValue());
+		Assertions.assertThat(result.match()).isEqualTo(1);
+		Assertions.assertThat(result.recall()).isEqualTo(1);
+		Assertions.assertThat(result.precision()).isEqualTo(1);
+	}
+
+	@Test
+	public void testMeasureFeedbackExactButLessPrecision() {
+		this.feedbackService.sugerirMatchingEvento(new FeedbackEventoForm(2L, "symposium 3d user interfaces", 2010));
+
+		MeasurementResult result = this.measurementService.measure(this.usuarioLogado, NivelSimilaridade.BAIXO);
+		// result.debug();
+
+		Assertions.assertThat(result.getMatches().get(0)).isEqualTo(true);
+		Assertions.assertThat(result.getPrecisions().get(0)).isEqualTo(0.5);
+		Assertions.assertThat(result.getRecalls().get(0)).isEqualTo(1);
+
+		Assertions.assertThat(result.size()).isEqualTo(1);
+		Assertions.assertThat(result.threshold()).isEqualTo(NivelSimilaridade.BAIXO.getValue());
+		Assertions.assertThat(result.match()).isEqualTo(1);
+		Assertions.assertThat(result.recall()).isEqualTo(1);
+		Assertions.assertThat(result.precision()).isEqualTo(0.5);
+	}
+
+	@Test
+	public void testMeasure() {
 		this.feedbackService.sugerirMatchingEvento(new FeedbackEventoForm(2L, "3d user interfaces conference", 2010));
 
-		for (float threshold = 0.1f; threshold <= 1.0; threshold += 0.1) {
-			MeasurementResult result = this.measurementService.measure(this.usuarioLogado, new NivelSimilaridade(threshold));
-			System.out.println(result);
-			// result.debug();
-		}
+		MeasurementResult result = this.measurementService.measure(this.usuarioLogado, NivelSimilaridade.BAIXO);
+		// result.debug();
+
+		Assertions.assertThat(result.getMatches().get(0)).isEqualTo(false);
+		Assertions.assertThat(result.getPrecisions().get(0)).isCloseTo(0.3333, Offset.offset(0.001));
+		Assertions.assertThat(result.getRecalls().get(0)).isEqualTo(1);
+
+		Assertions.assertThat(result.size()).isEqualTo(1);
+		Assertions.assertThat(result.threshold()).isEqualTo(NivelSimilaridade.BAIXO.getValue());
+		Assertions.assertThat(result.match()).isEqualTo(0);
+		Assertions.assertThat(result.recall()).isEqualTo(1);
+		Assertions.assertThat(result.precision()).isCloseTo(0.3333, Offset.offset(0.001));
 	}
 }
