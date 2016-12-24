2cb90d67c699e74adc03e05cb3a8e4d28b90d7eb
Carlos Bonetti <carlosb_bc@hotmail.com>
2016-12-24 13:12:23 -0200

    Mais opções de filtro para gráfico de número de publicações por ano

--- src/main/webapp/scripts/components/charts/chart-publicacoes-por-ano.directive.js
+++ src/main/webapp/scripts/components/charts/chart-publicacoes-por-ano.directive.js
@@ -9,15 +9,29 @@ angular.module('silq2App')
             },
             templateUrl: 'scripts/components/charts/chart-publicacoes-por-ano.html',
             link: function($scope) {
-                $scope.agrupador = 'todos';
-                $scope.grupoConceitual = 'A1,A2,B1';
+                $scope.agrupador = [
+                    { name: 'Artigos', selected: true },
+                    { name: 'Trabalhos', selected: true },
+                ];
+                $scope.grupos = [
+                    { name: 'A1', selected: true },
+                    { name: 'A2', selected: true },
+                    { name: 'B1', selected: true },
+                    { name: 'B2', selected: false },
+                    { name: 'B3', selected: false },
+                    { name: 'B4', selected: false },
+                    { name: 'B5', selected: false },
+                    { name: 'C',  selected: false }
+                ];
                 $scope.chart = {};
 
-                var reload = function() {
+                var reload = $scope.reload = function() {
                     var stats = $scope.stats;
                     var artigos = stats.publicacoesPorAno.artigos;
                     var trabalhos = stats.publicacoesPorAno.trabalhos;
-                    var grupos = $scope.grupoConceitual.split(',');
+                    var grupos = $scope.grupos
+                        .filter(function(g) { return g.selected; })
+                        .map(function(g) { return g.name; });
 
                     var chart = {};
                     chart.series = grupos;
@@ -37,15 +51,17 @@ angular.module('silq2App')
                             var countArtigos = parseInt(artigos[ano] && artigos[ano][serie] ? artigos[ano][serie] : 0);
                             var countTrabalhos = parseInt(trabalhos[ano] && trabalhos[ano][serie] ? trabalhos[ano][serie] : 0);
 
-                            if ($scope.agrupador === 'todos') {
-                                chart.data[i].push(countArtigos + countTrabalhos);
-                            } else if ($scope.agrupador === 'artigos') {
-                                chart.data[i].push(countArtigos);
-                            } else if ($scope.agrupador === 'trabalhos') {
-                                chart.data[i].push(countTrabalhos);
-                            } else {
-                                chart.data[i].push(0);
+                            var count = 0;
+                            for (var j in $scope.agrupador) {
+                                var a = $scope.agrupador[j];
+                                if (a.selected && a.name === 'Artigos') {
+                                    count += countArtigos;
+                                }
+                                if (a.selected && a.name === 'Trabalhos') {
+                                    count += countTrabalhos;
+                                }
                             }
+                            chart.data[i].push(count);
                         }
                     }
 
@@ -59,14 +75,6 @@ angular.module('silq2App')
                         reload();
                     }
                 });
-
-                $scope.changeGrupoConceitual = function() {
-                    reload();
-                };
-
-                $scope.changeAgrupador = function() {
-                    reload();
-                };
             }
         };
     });
